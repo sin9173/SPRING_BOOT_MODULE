fc37a25a5abc66a01043472248555cc2ff7e0f32
sin9173 <54428255+sin9173@users.noreply.github.com>
2022-09-07 11:34:18 +0900

    modules init

---
+++ src/main/java/com/util/modules/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.util.modules;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ModulesApplication.class);
+	}
+
+}
